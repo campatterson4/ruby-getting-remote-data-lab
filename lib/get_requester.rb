896561afea4adef9a677631e9c51aaddb28c4fb9
Camryn <camrynpatterson@yahoo.com>
2021-01-02 11:41:31 -0600

    commit tree a7e743b855a48afbab99c336dc7d6f1a3bce7940

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'net/http'
+ require 'open-uri'
+ require 'json'
+ require 'pry'
+
+class GetRequester
+    attr_reader :uri
+    def initialize(url)
+        @uri = URI.parse(url)
+    end
+
+    def get_response_body
+        response = Net::HTTP.get_response(self.uri)
+        response.body
+    end
+
+    def parse_json
+        json_parsed = JSON.parse(self.get_response_body)
+    end
+end 
